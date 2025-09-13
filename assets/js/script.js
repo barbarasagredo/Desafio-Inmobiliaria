d993000d4dcc4ca77c683340bd17cd5eaea6127a
Bárbara Sagredo <barpaz.sc@gmail.com>
2025-09-13 19:33:17 -0300

    iconos de mascotas y fumar permitidas y no admitidas se modifican condicionalmente

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,4 @@
-const propiedades_alquiler = [
-  {
-    nombre: "Apartamento en el centro de la ciudad",
-    src: "https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXBhcnRtZW50fGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60",
-    descripcion:
-      "Este apartamento de 2 habitaciones está ubicado en el corazón de la ciudad, cerca de todo.",
-    ubicacion: "123 Main Street, Anytown, CA 91234",
-    habitaciones: 2,
-    baños: 2,
-    valor: 2000,
-    smoke: false,
-    pets: true,
-  },
-  {
-    nombre: "Apartamento luminoso con vista al mar",
-    src: "https://images.unsplash.com/photo-1669071192880-0a94316e6e09?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
-    descripcion:
-      "Este hermoso apartamento ofrece una vista impresionante al mar",
-    ubicacion: "456 Ocean Avenue, Seaside Town, CA 56789",
-    habitaciones: 3,
-    baños: 3,
-    valor: 2500,
-    smoke: true,
-    pets: true,
-  },
-  {
-    nombre: "Condominio moderno en zona residencial",
-    src: "https://images.unsplash.com/photo-1567496898669-ee935f5f647a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNvbmRvfGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=1000&q=60",
-    descripcion:
-      "Este elegante condominio moderno está ubicado en una tranquila zona residencial",
-    ubicacion: "123 Main Street, Anytown, CA 91234",
-    habitaciones: 2,
-    baños: 2,
-    valor: 2200,
-    smoke: false,
-    pets: false,
-  },
-];
-
+// PROPIEDADES EN VENTA
 const propiedades_venta = [
   {
     nombre: "Apartamento de lujo en zona exclusiva",
@@ -77,10 +39,10 @@ const propiedades_venta = [
 ];
 
 const forSale = document.getElementById("for-sale-properties");
-let html = "";
-
-for (let property of propiedades_venta) {
-  html += `
+if (forSale) {
+  let htmlSale = "";
+  for (let property of propiedades_venta) {
+    htmlSale += `
     <div class="col-md-4 mb-4">
       <div class="card h-100">
         <img src="${
@@ -97,20 +59,101 @@ for (let property of propiedades_venta) {
           <p><i class="fas fa-dollar-sign"></i> ${property.valor.toLocaleString(
             "es-CO"
           )}</p>
-          <p class=" ${
-            property.smoke === false ? "text-danger" : "text-success"
-          } "><i class="fas fa-smoking-ban"></i> ${
-    property.smoke === false ? "No se permite fumar" : "Permitido fumar"
-  }</p>
-          <p class=${
-            property.pets === false ? "text-danger" : "text-success"
-          }><i class="fa-solid fa-ban"></i>${
-    property.pets === false ? " No se permiten mascotas" : " Mascotas permitidas"
-  }</p>
+          <p class="${property.smoke ? "text-success" : "text-danger"}">
+            <i class="${
+              property.smoke ? "fas fa-smoking" : "fas fa-smoking-ban"
+            }"></i> 
+            ${property.smoke ? "Permitido fumar" : "No se permite fumar"}
+          </p>
+          <p class="${property.pets ? "text-success" : "text-danger"}">
+            <i class="${property.pets ? "fas fa-paw" : "fa-solid fa-ban"}"></i> 
+            ${property.pets ? "Mascotas permitidas" : "No se permiten mascotas"}
+          </p>
         </div>
       </div>
     </div>
   `;
+  }
+
+  forSale.innerHTML = htmlSale;
 }
 
-forSale.innerHTML = html;
+// PROPIEDADES EN ALQUILER
+const propiedades_alquiler = [
+  {
+    nombre: "Apartamento en el centro de la ciudad",
+    src: "https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXBhcnRtZW50fGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60",
+    descripcion:
+      "Este apartamento de 2 habitaciones está ubicado en el corazón de la ciudad, cerca de todo.",
+    ubicacion: "123 Main Street, Anytown, CA 91234",
+    habitaciones: 2,
+    baños: 2,
+    valor: 2000,
+    smoke: false,
+    pets: true,
+  },
+  {
+    nombre: "Apartamento luminoso con vista al mar",
+    src: "https://images.unsplash.com/photo-1669071192880-0a94316e6e09?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    descripcion:
+      "Este hermoso apartamento ofrece una vista impresionante al mar",
+    ubicacion: "456 Ocean Avenue, Seaside Town, CA 56789",
+    habitaciones: 3,
+    baños: 3,
+    valor: 2500,
+    smoke: true,
+    pets: true,
+  },
+  {
+    nombre: "Condominio moderno en zona residencial",
+    src: "https://images.unsplash.com/photo-1567496898669-ee935f5f647a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNvbmRvfGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=1000&q=60",
+    descripcion:
+      "Este elegante condominio moderno está ubicado en una tranquila zona residencial",
+    ubicacion: "123 Main Street, Anytown, CA 91234",
+    habitaciones: 2,
+    baños: 2,
+    valor: 2200,
+    smoke: false,
+    pets: false,
+  },
+];
+
+const forRent = document.getElementById("for-rent-properties");
+let htmlRent = "";
+if (forRent) {
+  for (let property of propiedades_alquiler) {
+    htmlRent += `
+    <div class="col-md-4 mb-4">
+      <div class="card h-100">
+        <img src="${
+          property.src
+        }" class="card-img-top" alt="Imagen del departamento" />
+        <div class="card-body">
+          <h5 class="card-title">${property.nombre}</h5>
+          <p class="card-text">${property.descripcion}</p>
+          <p><i class="fas fa-map-marker-alt"></i> ${property.ubicacion}</p>
+          <p>
+            <i class="fas fa-bed"></i> ${property.habitaciones} Habitaciones |
+            <i class="fas fa-bath"></i> ${property.baños} Baños
+          </p>
+          <p><i class="fas fa-dollar-sign"></i> ${property.valor.toLocaleString(
+            "es-CO"
+          )}</p>
+          <p class="${property.smoke ? "text-success" : "text-danger"}">
+            <i class="${
+              property.smoke ? "fas fa-smoking" : "fas fa-smoking-ban"
+            }"></i> 
+            ${property.smoke ? "Permitido fumar" : "No se permite fumar"}
+          </p>
+          <p class="${property.pets ? "text-success" : "text-danger"}">
+            <i class="${property.pets ? "fas fa-paw" : "fa-solid fa-ban"}"></i> 
+            ${property.pets ? "Mascotas permitidas" : "No se permiten mascotas"}
+          </p>
+        </div>
+      </div>
+    </div>
+  `;
+  }
+
+  forRent.innerHTML = htmlRent;
+}
